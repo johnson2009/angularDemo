e8c525ea0b23274b9d34c3f4aa9a8328f57de239
Johnson Sun <Johnson.Sun@mkcorp.com>
2023-04-29 09:00:50 +0800

    Add HeroService

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes :Hero[]= [];
+
+  constructor(private heroService: HeroService){}
+
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero):void{
+    this.selectedHero = hero;
+  }
+}
